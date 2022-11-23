649152cc549349ce2f5b3d70ed1de49b6145756b
Gaston Sosa <gastongsosa@gmail.com>
2022-11-22 23:37:45 -0300

    Segundo commit clase 7

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
-import ItemListContainer from './components/ItemListContainer';
 import ItemCount from './components/ItemCount';
 import ItemDetailContainer from './components/ItemDetailContainer';
 
